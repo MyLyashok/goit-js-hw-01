bafd1f4842f86f46cc2429b38dba691f567b437c
Mykola Lyashok <lyashok100@gmail.com>
2026-04-04 21:32:56 +0300

    readme test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # goit-js-hw-01
+
+[![My Skills](https://skillicons.dev/icons?i=java,kotlin,nodejs,figma&theme=light)](https://skillicons.dev)
--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,35 @@
-function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
-}
+// function makeTransaction(quantity, pricePerDroid) {
+//     const totalPrice = quantity * pricePerDroid;
+//     return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+// }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+
+// function buyTickets(event, ticketPrice, quantity) {
+//     return `You bought ${quantity} tickets for ${event}, total cost ${ticketPrice * quantity} credits`;
+// }
+
+// function orderPizza(size, price, toppingsCount) {
+//     return `Your ${size} pizza with ${toppingsCount} toppings costs ${price + toppingsCount * 10} credits`;
+// }
+// function enrollStudent(courseName, basePrice, discount) {
+
+// }
+
+// function checkNumber(num) {
+//     if (num > 0) {
+//         return 'Positive';
+//     } else {
+//         return 'Negative or zero';
+//     }
+// }
+
+// console.log(checkNumber(5));
+// console.log(checkNumber(-3));
+
+
+
+
